03ad345475c8d0c0bfe9394a5d8bced5b5286ce9
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-03-26 20:11:04 +0200

    Added form and table

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,20 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+    $scope.table = [];
+
+    var line;
+
+    $scope.addRow = function () {
+      line = {
+        name: $scope.name,
+        firstname: $scope.firstname,
+        email: $scope.email
+      };
+
+      $scope.table.push(line);
+
+      $scope.name = '';
+      $scope.firstname = '';
+      $scope.email = '';
+    };
 }
